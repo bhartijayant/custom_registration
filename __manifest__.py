e2f44eb85177f61ddb030cabf0a172aa3a5ed509
bhartijayant <39701238+bhartijayant@users.noreply.github.com>
2024-11-18 18:56:48 +0530

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Registration Form',
+    'version': '1.0',
+    'summary': 'Basic Registration Form Module',
+    'author': 'Your Name',
+    'category': 'Custom',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/registration_form_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
